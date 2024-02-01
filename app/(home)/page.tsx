02a3a78159c03cacb0f0de7bab9aea50728e6b80
Matheus-santos7 <matheussantosgc@icloud.com>
2024-02-01 20:27:35 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Header from "../_components/hearder";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
-  return (
-    <div>
-      <Header/>
-    </div>
-  );
+    return (
+      <div>
+        <Header/>
+        <div className="px-5 pt-5">
+          <h2 className="text-xl font-bold">Olá, Matheus!</h2>
+          <p className="capitalize text-sm">
+            {format(new Date(),"EEEE', 'dd' de 'MMMM", {
+            locale: ptBR,})}
+          </p>
+        </div>
+      </div>
+    );
 }
