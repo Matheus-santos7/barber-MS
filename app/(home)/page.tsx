f9314f4a77da4824ad80aee3e440d3a37c78c9b5
Matheus-santos7 <matheussantosgc@icloud.com>
2024-02-01 23:54:23 -0300

    feat: add initial booking components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,24 +2,30 @@ import Header from "../_components/hearder";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() {
-    return (
-      <div>
-        <Header/>
+  return (
+    <div>
+      <Header />
 
-        <div className="px-5 pt-5">
-          <h2 className="text-xl font-bold">Olá, Matheus!</h2>
-          <p className="capitalize text-sm">
-            {format(new Date(),"EEEE', 'dd' de 'MMMM", {
-            locale: ptBR,})}
-          </p>
-        </div>
-        
-        <div className="px-5 mt-6">
-        <Search />
-        </div>
-        
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Matheus!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE', 'dd' de 'MMMM", {
+            locale: ptBR,
+          })}
+        </p>
       </div>
-    );
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold ">Agendamentos</h2>
+        <BookingItem />
+      </div>
+    </div>
+
+  );
 }
