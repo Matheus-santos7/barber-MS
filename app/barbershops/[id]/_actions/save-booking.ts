bb3b037d683ecd989946e1f6776225475f0125b1
Matheus-santos7 <matheussantosgc@icloud.com>
2024-02-06 20:06:15 -0300

    feat: add service-item components

---
+++ app/barbershops/[id]/_actions/save-booking.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { revalidatePath } from "next/cache";
+
+interface SaveBookingParams {
+  barbershopId: string;
+  serviceId: string;
+  userId: string;
+  date: Date;
+}
+
+export const saveBooking = async (params: SaveBookingParams) => {
+  await db.booking.create({
+    data: {
+      serviceId: params.serviceId,
+      userId: params.userId,
+      date: params.date,
+      barbershopId: params.barbershopId,
+    },
+  });
+
+  revalidatePath("/");
+  revalidatePath("/bookings");
+};
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
@@ -41,7 +40,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
 
             <div className="px-5 flex flex-col gap-4 py-6">
             {barbershop.services.map((service: { id: Key | null | undefined; }) => (
-                <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
+                <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user}/>
             ))}
             </div>
         </div>
